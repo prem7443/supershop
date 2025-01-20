796ea2e08fccc01a21ac9010f17475d0d70a84b4
prem7443 <123341381+prem7443@users.noreply.github.com>
2025-01-20 13:23:43 +0530

    Update settings.py

--- supershop/settings.py
+++ supershop/settings.py
@@ -132,8 +132,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "karmadjango@gmail.com"
-EMAIL_HOST_PASSWORD = "zpsdglxooqiowhms"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 
 RAZORPAY_API_KEY = 'rzp_test_kJFCr5jnzPYy9s'
