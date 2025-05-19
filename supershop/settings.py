dbfcab0dba9f083ce28424481d86d08d6d291554
prem7443 <123341381+prem7443@users.noreply.github.com>
2025-05-19 11:31:00 +0530

    Update settings.py

--- supershop/settings.py
+++ supershop/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
